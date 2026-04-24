19c326c3b30ff4aa1d72a0df1367e7d50701659f
mahakyadav13-code <mahakyadav1309@gmail.com>
2026-04-24 13:17:36 +0530

    frintent api fix

--- webtech/frontend/js/api.js
+++ webtech/frontend/js/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://web-technology-project-hoho.onrender.com/api';
 
 // Store token
 function setToken(token) {
