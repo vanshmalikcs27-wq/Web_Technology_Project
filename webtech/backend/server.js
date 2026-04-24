75f33ae9651f2f6b4af8bb44ebb75601a16a6d0c
mahakyadav13-code <mahakyadav1309@gmail.com>
2026-04-24 13:07:22 +0530

    requiremts add

--- webtech/backend/server.js
+++ webtech/backend/server.js
@@ -15,10 +15,16 @@ const { initializeDatabase } = require('./db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("API working 🚀");
+});
 
 // Serve static files from frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
